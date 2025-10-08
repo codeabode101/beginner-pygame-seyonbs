dcb0d9a349634b62c42898ec9ebddb709076718e
seyonbs <seyonbs@gmail.com>
2025-10-07 20:14:19 -0400

    Add files via upload

--- Game_window.py
+++ Game_window.py
@@ -6,16 +6,17 @@ WIDTH =  800
 pygame.display.set_caption('my first window')
 running = True
 while running:
-    keys = keys =  pygame.key.get_pressed()
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         screen.fill((255,0,0))    
-    screen.fill((180,180,180))            
+    screen.fill((180,180,180))
+    if keys[pygame.K_c]:
+        screen.fill(66,250,0)            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        user_input = input("you can press 'Q' to quit")
-        if user_input == "Q":
-            break                          
+        if keys[pygame.K_q]:
+            pygame.quit()                              
 # Expected Output (when running the code):
 # A black window titled "My First Window" appears.
 # Clicking the 'X' and 'Q'(keyboard) button on the window closes it.
